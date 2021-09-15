2d3c4a798acf7cb8555fbd07105a17d96a4c1a71
ToshikiKawata <jubei.animal@gmail.com>
2021-09-15 21:46:14 +0900

    計算アプリ課題完了

--- app/Http/Controllers/calccontroller.php
+++ app/Http/Controllers/calccontroller.php
@@ -7,19 +7,21 @@ use Illuminate\Http\Request;
 
 class calccontroller extends Controller
 {
-    public function result ($num1, $operator, $num2) {
-    if ( $operator == 'addition' ) {
-        $result = $num1 + $num2;
-    } elseif ( $operator == 'subtraction' ) {
-        $result = $num1 - $num2;
-    } elseif ( $operator == 'multiplication' ) {
-        $result =  $num1 * $num2;
-    } elseif ( $operator == 'division' ) {
-        $result = $num1 / $num2;
-    } else {
-        $result = "数字なし";
+    public function result($num1, $operator, $num2)
+    {
+        if ($operator == 'addition') {
+            $result = $num1 + $num2;
+        } elseif ($operator == 'subtraction') {
+            $result = $num1 - $num2;
+        } elseif ($operator == 'multiplication') {
+            $result =  $num1 * $num2;
+        } elseif ($operator == 'division') {
+            $result = $num1 / $num2;
+        } else {
+            $result = "数字なし";
+        }
+        return view('message.calc', [
+            'result' => $result
+        ]);
     }
-    return view('message.calc', [
-        'result' => $result
-    ]);
-}}
+}
